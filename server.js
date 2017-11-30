dec3a60e4508817062b0c23e7f56e7ccb28e0df1
Erik Waters <erikj.waters@gmail.com>
2017-11-30 15:06:55 -0500

    cleanup

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-"use strict";
+"use strict"
 
 const path = require("path"),
     express = require("express"),
@@ -7,16 +7,16 @@ const path = require("path"),
 
 app.set("port", process.env.PORT || 8000)
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname))
 
 ssMain.ioServer(app).listen(app.get("port"), () => {
     console.log("listening to port 8k")
-});
+})
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname + '/src/client/views/index.html'))
-});
+})
 
 app.get("/restapi", (req, res) => {
     res.sendFile(path.join(__dirname + '/src/client/views/restapi.html'))
-});
+})
--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,6 @@
 "use strict"; 
 // create IO server instance
 let ioServer = app => {
-    // app.locals.tools = [];
     const server = require("http").Server(app);
     const io = require("socket.io")(server);
     require("./socket")(io, app);
@@ -10,6 +9,5 @@ let ioServer = app => {
 };
 
 module.exports = {
-    router: require("./routes"),
     ioServer
 }
