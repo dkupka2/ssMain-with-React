945a5b9623f8335b26f5b918d35e1f8d85bd5e5c
Erik Waters <erikj.waters@gmail.com>
2017-11-30 15:02:44 -0500

    routing schema not adding any value; removed it

--- server.js
+++ server.js
@@ -2,14 +2,10 @@
 
 const path = require("path"),
     express = require("express"),
-    { createEngine } = require('express-react-views'),
     app = express(),
     ssMain = require("./src/server")
 
 app.set("port", process.env.PORT || 8000)
-app.set("view engine", "jsx")
-app.engine('jsx', createEngine())
-app.set("views", path.join(__dirname, '/src/client/views'))
 
 app.use(express.static(__dirname));
 
@@ -17,6 +13,10 @@ ssMain.ioServer(app).listen(app.get("port"), () => {
     console.log("listening to port 8k")
 });
 
-app.use("/", (req, res) => {
-    ssMain.router(req, res)
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + '/src/client/views/index.html'))
+});
+
+app.get("/restapi", (req, res) => {
+    res.sendFile(path.join(__dirname + '/src/client/views/restapi.html'))
 });
