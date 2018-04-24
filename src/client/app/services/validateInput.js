f1592622c452023de1fdb279b7385d0967e51aa6
Erik Waters <erikj.waters@gmail.com>
2018-04-24 12:13:17 -0400

    clean up

--- src/client/app/services/validateInput.js
+++ src/client/app/services/validateInput.js
@@ -1,13 +1,17 @@
-export const validateInput = (val) => {
+export const validateInput = val => {
     let arr
     if (val) {
-        // if length is valid and last char is a number...
-        arr = Array.from(val) 
-        if ( arr.length < 5 && ! isNaN( parseInt(arr[arr.length-1], 10) ) ) {
-            return val.slice() 
+        arr = Array.from(val)
+        // if length is valid and last char is a number
+        if (
+            arr.length < 5 &&
+            ! isNaN( parseInt(arr[arr.length-1], 10)
+        ) ) {
+            // return string
+            return val.slice()
         } else {
-            // return string else return string without invalid char
+            // else return string without invalid char
             return val.slice(0, val.length-1)
         }
-    } else return ''
+    } return ''
 }
