1e52530f533d20abf8fff07047a47b7885a5bcdb
Erik Waters <erikj.waters@gmail.com>
2018-06-15 18:55:19 -0400

    delegated row creation to combineRow fn

--- src/client/app/store/utils/loadCache.js
+++ src/client/app/store/utils/loadCache.js
@@ -1,38 +1,44 @@
 import { applyView } from '../'
 import { getLastArraY } from '../../services'
 
+const filterRow = body => target => array => table => [
+    ...body,
+    ...applyView(target)( getLastArray(array) )(table)
+]
+
 export const loadFromCache = data => applyView => {
-    data = { ...data }
-    let { type, acct, optTable, accts } = data,
-        targetArray, body = []
+    let targetArray,
+        body = [],
+        { type, acct, optTable, accts } = data
+
     if ( tables.lists.compound.includes(optTable) ) {
         // map over tables list
         tables.compoundLists[optTable].map( targetTable => {
-            // filter and concat most recent cache
+            // revert table name to human readable
             targetTable = tables.revertKeys[targetTable]
             // cacheData from target table
             targetArray = accts[acct][targetTable]
             // if cache has data
             if (targetArray.length > 0) {
                 // filter and aggregate data
-                body = [
-                    ...body,
-                    ...(applyView(targetTable)
-                        (getLastArray(targetArray))
-                        (optTable)
-                    )
-                ]
+                body = filterRow(body)
+                    (targetTable)
+                    (targetArray)
+                    (optTable)
             }
         })
     } else {
+        // cacheData from selected table
+        targetArray = accts[acct][optTable]
         // if table data exists in accts.acct[table]
-        if (accts[acct][optTable].length > 0) {
+        if (targetArray.length > 0) {
             // coerce viewTable to table value for single document tables
             if (! viewTable) viewTable = table
             // filter table
-            body = body.concat(
-                applyView(optTable)(getLastArray( accts[acct][optTable]))
-            )
+            body = filterRow(body)
+                (optTable)
+                (targetArray)
+                (optTable)
         }
     }
     // strip out undefined / null entries
