f24f7349be31859c9c31a3b122239576d47eab2e
Erik Waters <erikj.waters@gmail.com>
2018-09-06 13:28:51 -0400

    testing for single table requests via callAPI.js

--- src/client/app/store/__tests__/callAPI.test.js
+++ src/client/app/store/__tests__/callAPI.test.js
@@ -2,20 +2,94 @@ const assert = require("chai").assert;
 import { callAPI_piRest } from "../";
 
 const fail = "callAPI did not return";
-const acct = 1;
-const typeCompound = "compound";
-const typeSingle = "single";
-const view = "view";
-let tables = {};
 
-let socket = {};
-socket.events = []
-socket.emit = (event, data) => {
-  return { event, data };
+let acct, type, view;
+let tables = {};
+let socket = {
+  events: [],
+  emit(event, data) {
+    socket.events = [...socket.events, { event, data }];
+    return socket.events;
+  }
 };
 
-describe("callAPI", () => {
-  it("calls socket.emit if type is not 'compound'", () => {
-    assert.equal(callAPI_piRest());
+describe("callAPI - single table tests", () => {
+  beforeEach(() => {
+    acct = 1;
+    type = "single";
+    view = "view";
+    tables = {
+      requestKeys: {
+        single: "request key"
+      },
+      single: {
+        view: "view name"
+      }
+    };
+    callAPI_piRest(acct)(type)(view)(tables)(socket);
   });
+  afterEach(() => {
+    socket.events = [];
+  });
+  it("calls emit once if type is not 'compound'", () => {
+    assert.equal(
+      socket.events.length,
+      1,
+      `${fail} a single-table request event`
+    );
+  });
+  it("emits a single-table request event with expected props/vals", () => {
+    assert.equal(
+      socket.events[0].data.acct,
+      1,
+      `${fail} a single-table request event with expected acct value`
+    );
+    assert.equal(
+      socket.events[0].data.table,
+      "view name",
+      `${fail} a single-table request event with expected table value`
+    );
+    assert.equal(
+      socket.events[0].event,
+      "request key",
+      `${fail} a single-table request event with expected event value`
+    );
+  });
+});
+
+describe("callAPI - compound table tests", () => {
+  beforeEach(() => {
+    acct = 1;
+    type = "compound";
+    view = "test";
+    tables = {
+      requestKeys: {
+        local: "REQUEST_LOCAL",
+        global: "REQUEST_GLOBAL"
+      },
+      compound: {
+        test: {
+          local: ["localOne", "localTwo"],
+          global: ["globalOne"]
+        },
+        control: {
+          local: [" "]
+        }
+      }
+    };
+    callAPI_piRest(acct)(type)(view)(tables)(socket);
+  });
+  afterEach(() => {
+    socket.events = [];
+  });
+  it("calls emit for each doc in the selected compound view", () => {
+    assert.equal(
+      socket.events.length,
+      3,
+      `${fail} a compound table request with the right number of events`
+    );
+  });
+  // it("emits a request event for each table with expected props/vals", () => {
+
+  // })
 });
